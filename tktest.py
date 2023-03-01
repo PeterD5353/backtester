19141e2eb433ac58a84a84b487d507cd089a77f0
PeterD5353 <58152012+PeterD5353@users.noreply.github.com>
2023-03-01 10:40:53 -0500

    Add elements to GUI

--- tktest.py
+++ tktest.py
@@ -1,8 +1,46 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+from tkcalendar import DateEntry
 
 root = tk.Tk()
 
-root.geometry("800x1200")
+root.geometry("1200x800")
+root.title("Backtester")
+
+label = tk.Label(root, text="Select Security and Strategy", font=("ariel", 18))
+label.pack(pady = 10)
+
+frame = tk.Frame(root)
+frame.pack()
+
+# get security info
+infoFrame = tk.LabelFrame(frame, text = "Strategy Information")
+infoFrame.grid(row=0, column=0)
+
+
+
+securityLabel = tk.Label(infoFrame, text="Security")
+securityLabel.grid(row = 0, column = 0)
+strategyLabel = tk.Label(infoFrame, text="Strategy")
+strategyLabel.grid(row = 0, column=1)
+
+# enter security and select strategy
+securityEntry = tk.Entry(infoFrame)
+options = StringVar()
+options.set("Strategy")
+strategySelect = OptionMenu(infoFrame, options, "RSI", "MA Crossover", "MA")
+securityEntry.grid(row=1, column=0)
+strategySelect.grid(row=1,column=1)
+
+# start date calendar
+cal = DateEntry(infoFrame, selectmode= "day", year=2021, month=12, day=31)
+cal.grid(row=2, column=0)
+
+
+
+
+# button pressed
 
 root.mainloop()
 
